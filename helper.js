fb3e4853b0c2a47d1babc5f3d91bf034149ab887
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-21 11:52:20 +0530

    added

--- helper.js
+++ helper.js
@@ -1,10 +1,10 @@
 import  {client} from "./index.js";
 
-// export async function getAllRooms(request) {
-//  return await client.db("Rooms")
-//  .collection("Rooms")
-//  .find(request.query).toArray();
-// }
+export async function getAllRooms(request) {
+ return await client.db("Rooms")
+ .collection("Rooms")
+ .find(request.query).toArray();
+}
 
 export async function createNewRooms(data){
  return await client.db("Rooms")
